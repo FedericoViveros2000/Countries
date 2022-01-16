fa793592f47f2d29b30c3420d620a35c26a2120d
Josfe <josevivers2000@gmail.com>
2022-01-16 19:41:35 -0300

    deploy__1/16/2022

---
+++ precache-manifest.c7bcdc6025d4547dea8863e246830ba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd33b2be844725dad16",
+    "url": "/Countries/css/app.232b986c.css"
+  },
+  {
+    "revision": "9ca12eb7b1ea5179641f9a2d52b6ba07",
+    "url": "/Countries/index.html"
+  },
+  {
+    "revision": "afd33b2be844725dad16",
+    "url": "/Countries/js/app.7188df46.js"
+  },
+  {
+    "revision": "67350a17a982333d22be",
+    "url": "/Countries/js/chunk-vendors.7ec7b66a.js"
+  },
+  {
+    "revision": "4a9626ce434fe695abfed77a3a2f9d71",
+    "url": "/Countries/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Countries/robots.txt"
+  }
+]);
